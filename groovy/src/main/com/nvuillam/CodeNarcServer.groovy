f23850e7ed9a513b5d57519ac0398788e80a71f2
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-03-02 02:36:32 +0100

    CodeNarc Server v0.1

--- groovy/src/main/com/nvuillam/CodeNarcServer.groovy
+++ groovy/src/main/com/nvuillam/CodeNarcServer.groovy
@@ -1,5 +1,8 @@
 package com.nvuillam
 
+import com.sun.net.httpserver.HttpServer
+import com.sun.net.httpserver.HttpExchange
+
 import org.codenarc.CodeNarc
 
 /*
@@ -9,14 +12,50 @@ import org.codenarc.CodeNarc
  */
 class CodeNarcServer {
 
+    int PORT = 7484 
+
     /**
      * Main command-line entry-point. Run the CodeNarcServer application.
      * @param args - the String[] of command-line arguments
      */
     static void main(String[] args) {
-
+        println 'CodeNarcServer: '+args
+        final List<String> argsList =  new ArrayList<String>()
+        Collections.addAll(argsList, args); 
         CodeNarcServer server = new CodeNarcServer()
-        server.runCodeNarc(args);
+        if (argsList.contains('--server')) {
+            server.initialize()
+            argsList.remove('--server')
+            argsList.remove('--port')
+        }
+        if (['-basedir','-includes','-excludes','-rulesetfiles','-report','-help'].intersect(argsList)) {
+            server.runCodeNarc((String[])argsList)
+        }
+    }
+
+    void initialize() {
+        HttpServer.create(new InetSocketAddress(PORT), /*max backlog*/ 0).with {
+            println "Server is listening on ${PORT}, hit Ctrl+C to exit."    
+            createContext("/") { http ->
+                println "INIT: Hit from Host: ${http.remoteAddress.hostName} on port: ${http.remoteAddress.holder.port}"
+                try {
+                    def queryParams = getQueryParameters(http)
+                    println queryParams
+                    def codenarcArgs = queryParams.codenarcargs 
+                    def codenarcArgsArray = codenarcArgs.split(" ")
+                    runCodeNarc(codenarcArgsArray)
+                    http.responseHeaders.add("Content-type", "text/plain")
+                    http.sendResponseHeaders(200, 0)
+                    http.responseBody.withWriter { out ->
+                        out << "Hello ${http.remoteAddress.hostName}!\n"
+                }
+                println "Hit from Host: ${http.remoteAddress.hostName} on port: ${http.remoteAddress.holder.port}"
+                } catch (Throwable t) {
+                    t.printStackTrace()
+                }
+            }
+            start()
+        }
     }
 
     protected runCodeNarc(String[] args) {
@@ -27,6 +66,22 @@ class CodeNarcServer {
         catch(Throwable t) {
             println "CodeNarcServer ERROR: ${t.toString()}"
             t.printStackTrace()
+        }
+    }
+
+    private Map<String,String> getQueryParameters( HttpExchange httpExchange ) {
+        def query = httpExchange.getRequestURI().getQuery()
+        return query.split( '&' )
+                .collectEntries {
+            String[] pair = it.split( '=' )
+            if (pair.length > 1)
+            {
+                return [(pair[0]): pair[1]]
+            }
+            else
+            {
+                return [(pair[0]): ""]
+            }
         }
     }
 }
