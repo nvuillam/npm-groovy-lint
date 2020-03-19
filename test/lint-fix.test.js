50430fd375f86de3af2feeca0d16a51f81441fdb
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-03-19 15:43:11 +0100

    and again

--- test/lint-fix.test.js
+++ test/lint-fix.test.js
@@ -8,11 +8,10 @@ const rimraf = require("rimraf");
 
 // Copy files in temp directory to not update the package files
 async function copyFilesInTmpDir() {
-    const rootTmpDir = (process.env.CI === 'true' && process.env.CIRCLECI === 'true') ?
-        '/root/project/tmp' : (
-            (os.type().toLowerCase().includes('linux')) ?
-                '/tmp' :
-                os.tmpdir());
+    const rootTmpDir =
+        (os.type().toLowerCase().includes('linux')) ?
+            './jdeploy-bundle/tmptest' :
+            os.tmpdir();
     const tmpDir = rootTmpDir + '/' + ('tmpGroovyLintTest_' + Math.random()).replace('.', '');
     await fse.ensureDir(tmpDir, { mode: '0777' });
     await fse.copy('./jdeploy-bundle/lib/example', tmpDir);
