57059208b5a3cfc71e24444ec571ba81b148013c
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2020-07-23 18:55:48 +0200

    Parse all groovy files to lint (#74)

---
+++ lib/example/test/groovy-bad.groovy
@@ -0,0 +1,9 @@
+class Example {
+   static void main(String[] args) {
+      File file = new File("E:/Example.txt")
+      println "The file ${file.absolutePath} has ${file.length()} bytes"
+   }
+}
+class Example{
+some noise here
+}
