305b3591762bc5678138fb6814b0bf4b48750842
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2022-08-12 22:44:34 +0200

    change test class result error + test ranges

--- test/lint-range.rules.test.js
+++ test/lint-range.rules.test.js
@@ -9,47 +9,43 @@ const { beforeEachTestCase, checkCodeNarcCallsCounter, getDiff } = require("./he
 const npmGroovyLintRules = getNpmGroovyLintRules({ loadTests: true });
 
 // Read rules file and test all fixes
-describe("Check rules auto-fixes", () => {
+describe("Check range detection", () => {
     beforeEach(beforeEachTestCase);
 
     // Iterate all rules
     for (const ruleName of Object.keys(npmGroovyLintRules)) {
         let pos = 1;
         // Process only the rules with a fix defined
-        if (npmGroovyLintRules[ruleName].fix && npmGroovyLintRules[ruleName]) {
-            if (npmGroovyLintRules[ruleName].tests && npmGroovyLintRules[ruleName].tests.length > 0) {
+        if (npmGroovyLintRules[ruleName].rangeTests && npmGroovyLintRules[ruleName]) {
+            if (npmGroovyLintRules[ruleName].rangeTests && npmGroovyLintRules[ruleName].rangeTests.length > 0) {
                 // Process rule tests
-                for (const testRule of npmGroovyLintRules[ruleName].tests) {
+                for (const testRule of npmGroovyLintRules[ruleName].rangeTests) {
                     // Do not test non-codenarc rules, as they can't be returned by CodeNarc Linter
                     if (npmGroovyLintRules[ruleName].isCodeNarcRule == null || npmGroovyLintRules[ruleName].isCodeNarcRule === true)
                         it(`${ruleName} (${pos})`, async () => {
-                            await checkRuleFix(ruleName, testRule);
+                            await checkRuleRange(ruleName, testRule);
                         }).timeout(60000);
                     pos = pos + 1;
                 }
             } else {
                 // At least one rule should be defined
-                it(`${ruleName} fix tests has not been defined: please do it !`, async () => {
-                    assert(0 === 1, `${ruleName} fix tests has not been defined: please do it !`);
+                it(`${ruleName} range tests has not been defined: please do it !`, async () => {
+                    assert(0 === 1, `${ruleName} range tests has not been defined: please do it !`);
                 });
             }
         }
     }
 });
 
-// Check rule fix result
-async function checkRuleFix(ruleName, testRule) {
-    let fixRules = ruleName;
+// Check rule has a correct range detection
+async function checkRuleRange(ruleName, testRule) {
     // Call linter & fixer
-    const source = normalizeNewLines(testRule.sourceBefore);
+    const source = normalizeNewLines(testRule.source);
     const npmGroovyLintConfig = {
         source: source,
-        rulesets: fixRules,
-        fix: true,
-        fixrules: fixRules,
+        rulesets: ruleName,
         nolintafter: true,
         insight: false,
-        output: "none",
         verbose: true
     };
     let err = null;
@@ -64,8 +60,7 @@ async function checkRuleFix(ruleName, testRule) {
     // Check results
     assert(err == null, "No crash during NpmGroovyLint run");
     assert(linter.status === 0, `Linter status is 0 (${linter.status} returned)`);
-    const updatedSource = linter.lintResult.files[0].updatedSource;
-    const effectiveDiffs = getDiff(normalizeNewLines(testRule.sourceAfter), updatedSource, source);
-    assert(effectiveDiffs.length === 0, `Fix result is not the one expected.\nExpected:\n${normalizeNewLines(testRule.sourceAfter)}\nResult:\n${updatedSource}`);
+    const identifiedRange = linter.lintResult.files[0].errors[0].range;
+    assert(JSON.stringify(identifiedRange) === JSON.stringify(testRule.expectedRange), `Range result is not the one expected.\nExpected:\n${JSON.stringify(testRule.expectedRange)}\nResult:\n${identifiedRange}`);
     checkCodeNarcCallsCounter(testRule.codeNarcCallsNumber || 1);
 }
