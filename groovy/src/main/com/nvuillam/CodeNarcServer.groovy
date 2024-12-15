4d15bc9c32a2eb3de51b43457961f11d65d209d5
Angelos Mitsios <65828210+AngMits@users.noreply.github.com>
2024-12-15 02:11:57 +0200

    Updates to support groovy 4 (#439)
    * JAR updates and import change (#1)
    
    Bump jars to their latest versions
    Update the CodeNarcServer
    Update linter groovy version
    
    * Revert added space

--- groovy/src/main/com/nvuillam/CodeNarcServer.groovy
+++ groovy/src/main/com/nvuillam/CodeNarcServer.groovy
@@ -29,6 +29,9 @@ import com.fasterxml.jackson.databind.ObjectWriter
 // Groovy Transform
 import groovy.transform.CompileDynamic
 
+// Groovy CliBuilder change for Groovy 4
+import groovy.cli.commons.CliBuilder
+
 // Logging
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
