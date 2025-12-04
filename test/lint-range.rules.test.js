4838e333ad9e9c7f7f070e32abcb1379c86c0b8e
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2025-12-04 11:26:55 +0100

    rollback

--- test/lint-range.rules.test.js
+++ test/lint-range.rules.test.js
@@ -1,38 +1,15 @@
 #! /usr/bin/env node
 import NpmGroovyLint from "../lib/groovy-lint.js"
+import { getNpmGroovyLintRules } from "../lib/groovy-lint-rules.js";
 import { normalizeNewLines } from "../lib/utils.js";
-import assert from 'assert';
-import { readdirSync } from "fs";
-import path from "path";
-import { createRequire } from "module";
+import  assert from 'assert';
 
 import { beforeEachTestCase, checkCodeNarcCallsCounter } from "./helpers/common.js";
 
-const RULES_DIR = path.resolve(process.cwd(), "lib", "rules");
-const requireForRules = createRequire(import.meta.url);
-
-function loadRuleDefinitions() {
-    const rules = {};
-    const files = readdirSync(RULES_DIR);
-    for (const file of files) {
-        if (!file.endsWith(".js")) {
-            continue;
-        }
-        const ruleModule = requireForRules(path.join(RULES_DIR, file));
-        const rule = ruleModule?.rule;
-        if (!rule || rule.disabled) {
-            continue;
-        }
-        rules[file.replace(".js", "")] = rule;
-    }
-    return rules;
-}
-
-const npmGroovyLintRules = loadRuleDefinitions();
-
 // Read rules file and test all fixes
-describe("Check range detection", () => {
+describe("Check range detection", async () => {
     beforeEach(beforeEachTestCase);
+    const npmGroovyLintRules = await getNpmGroovyLintRules({ loadTests: true });
     // Iterate all rules
     for (const ruleName of Object.keys(npmGroovyLintRules)) {
         let pos = 1;
